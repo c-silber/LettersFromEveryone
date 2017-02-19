961440de28c40e21b0dbbd06150b35498ab5a6e6
Avigail <mazehle.ah@gmail.com>
2017-02-19 02:35:59 -0500

    Add about us file and view thing

--- APeckOfOwls/owls/views.py
+++ APeckOfOwls/owls/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	return HttpResponse("Hello, world. You're at the owls index.")
+
+def about_us(request):
+	return HttpResponse("We are a grassroots organization that sends postcards.")
